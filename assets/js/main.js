a0643c953db99baf626012e3dfb041cd09f021ff
ixyhoo <ixyhoo@outlook.fr>
2023-04-17 17:41:15 +0200

    switch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
-var age = prompt("how old are yopu?")
+var age = prompt();
+age = parseInt(age)
+switch (age) {
+    case 25:
+        console.log("you are 25 years old")
+        break;
+        case 28:
+        console.log("you are 28 years old")
+        break;
+        case 15:
+        console.log("you are 15 years old")
+        break;
 
-if (age > 18) {
-    console.log("you are an adult")
-}else{
-    console.log("you are a child")
+    default:
+        console.log("you are note 25, 28, 15 years")
+        break;
 }
-
-
