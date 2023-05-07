cc7f313eba76fa6ab534f9f3de0acdc723159f7b
ixyhoo <ixyhoo@outlook.fr>
2023-05-07 18:32:34 +0200

    make later

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,14 @@
 let listenerFunction = {
-    h2DblClick: () => {
-        window.alert("fly!")
+    h2Click: () => {
+        console.log(this);
         console.log("click")
     }
 }
 
 let setupListeners = () => {
     let h2 = document.querySelector("section#html h2")
-    h2.addEventListener("dblclick", listenerFunction.h2DblClick)
+    h2.addEventListener("click", listenerFunction.h2Click)
 }
+
+
 
